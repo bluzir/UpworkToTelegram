f77168f7048a1a1947d2845fee87be9808d44c3b
Vladislav <kouklevv@gmail.com>
2016-10-21 10:56:59 +0300

    Add states and method for getting new jobs

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import shelve
+
 import feedparser
 import requests
 
@@ -6,8 +8,9 @@ from settings import UPWORK_FEED_URL, TELEGRAM_BOT_TOKEN
 
 class RSSManager:
 
-    def __init__(self, url):
+    def __init__(self, url, state):
         self.url = url
+        self.state = state
         self.feed = None
 
     def parse_feed_by_url(self):
@@ -18,8 +21,17 @@ class RSSManager:
         except Exception as e:
             print(e)
 
-    def parse_jobs_from_feed(self):
-        pass
+    def get_new_jobs(self):
+        old_pubdate = self.state.get_value_from_key('pubdate')
+        new_pubdate = self.feed['pubdate']
+        if old_pubdate:
+            if old_pubdate == new_pubdate:
+                return []
+            else:
+                return []
+        else:
+            self.state.add_value_by_key('pubdate', new_pubdate)
+            return reversed(self.feed['entries'])
 
 
 class Job:
@@ -57,17 +69,38 @@ class TelegramAPIManager:
         print(response.json())
 
 
+class StateManager():
+    def __init__(self, file):
+        self.file = file
 
-my_upwork_feed = RSSManager(UPWORK_FEED_URL)
-my_telegram = TelegramAPIManager(-1001024228888)
-my_upwork_feed.parse_feed_by_url()
-if my_upwork_feed.feed:
-    jobs = my_upwork_feed.feed
-    for job in jobs:
-        title, published, link = job['title'], job['published'], job['link']
-        new_job = Job(title, published, link)
-        new_job.format_job_to_message()
-        my_telegram.send_message(new_job.formatted)
+    def add_value_by_key(self, key, value):
+        d = shelve.open(self.file)
+        d[key] = value
+        d.close()
+
+    def get_value_from_key(self, key):
+        d = shelve.open(self.file)
+        if d.has_key(key):
+            value = d[key]
+            return value
+        else:
+            return False
 
 
+def main():
+    state = StateManager('base.txt')
+    my_upwork_feed = RSSManager(UPWORK_FEED_URL, state)
+    my_telegram = TelegramAPIManager(-1001024228888)
+    my_upwork_feed.parse_feed_by_url()
+    while True:
+        jobs = my_upwork_feed.get_new_jobs()
+        for job in jobs:
+            title, published, link = job['title'], job['published'], job['link']
+            new_job = Job(title, published, link)
+            new_job.format_job_to_message()
+            my_telegram.send_message(new_job.formatted)
+            state.add_value_by_key('last_guid', job['guid'])
 
+
+if __name__ == '__main__':
+    main()
